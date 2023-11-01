48cbbbc5af74ada10c3e1584a4570217ce135474
Daiva <antoniusdaiva@gmail.com>
2023-11-01 23:37:13 +0700

    feat: Login function (with database conn)

--- SayangBayi/LoginPage.xaml.cs
+++ SayangBayi/LoginPage.xaml.cs
@@ -1,4 +1,3 @@
-﻿using SayangBayi.Classes;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Npgsql;
+using System.Data;
 
 namespace SayangBayi
 {
@@ -24,6 +25,45 @@ namespace SayangBayi
         public LoginPage()
         {
             InitializeComponent();
+            conn = new NpgsqlConnection(connstring);
+        }
+
+        private NpgsqlConnection conn;
+        string connstring = "Host=localhost;Port=5432;Username=postgres;Password=gajah;Database=sayangbayi";
+        public DataTable dt;
+        public static NpgsqlCommand cmd;
+        private string sql = null;
+
+        private bool Verify(string username, string password)
+        {
+            try 
+            {
+                conn.Open();
+                sql = $"SELECT * FROM users WHERE username='{username}' AND user_pass='{password}'";
+                cmd = new NpgsqlCommand(sql, conn);
+                NpgsqlDataReader rd = cmd.ExecuteReader();
+
+                bool found = false;
+                found = rd.HasRows;
+
+                rd.Close();
+                conn.Close();
+
+                return found;
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                conn.Close();
+                return false;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window window = Window.GetWindow(this);
+            window.Content = new RegisterPage();
         }
 
         private void BtnRegHere_Click(object sender, RoutedEventArgs e)
@@ -34,8 +74,18 @@ namespace SayangBayi
 
         private void BtnLogin_Click(object sender, RoutedEventArgs e)
         {
-            Window window = Window.GetWindow(this);
-            window.Content = new HomePage();
+            string username = usernameTBox.Text;
+            string password = passwordTBox.Text;
+            if (Verify(username, password))
+            {
+                Window window = Window.GetWindow(this);
+                window.Content = new HomePage();
+            }
+            else { MessageBox.Show("Incorrect Username or Password"); }
+
+            //klo mau skip login funct
+            //Window window = Window.GetWindow(this);
+            //window.Content = new HomePage();
         }
 
     }
